072a519f6132e434951f81f0befa8a79ea3b3bd6
christmattos <ra124994@uem.br>
2026-05-15 00:14:13 -0300

    alteracoes

---
+++ backend/app/Http/Controllers/Api/TaskController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Task;
+use Illuminate\Http\Request;
+
+class TaskController extends Controller
+{
+    public function index()
+    {
+        return Task::all();
+    }
+
+    public function store(Request $request)
+    {
+        $task = Task::create([
+            'title' => $request->title,
+            'done' => false
+        ]);
+
+        return $task;
+    }
+
+    public function update(Request $request, $id)
+    {
+        $task = Task::findOrFail($id);
+        $task->update($request->all());
+
+        return $task;
+    }
+
+    public function destroy($id)
+    {
+        Task::destroy($id);
+
+        return response()->json(['message' => 'deleted']);
+    }
+}
